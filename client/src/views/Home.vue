23430c67592ee1a5b8ce30365fe8cad73c6759ab
gitkyla <agitsyang@gmail.com>
2021-03-14 00:55:03 +0700

    refixing

---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TheNavbar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TheNavbar from "../components/TheNavbar";
+
+export default {
+  name: "Home",
+  components: {
+    TheNavbar,
+  },
+};
+</script>
